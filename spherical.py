7a59dd01f2fac947d03c30015e148b34327eaec4
Julia Piascik <98356859+liapia99@users.noreply.github.com>
2024-02-19 14:25:54 -0500

    Update spherical.py

--- spherical.py
+++ spherical.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from rplidar import RPLidar, RPLidarException
 
 def get_data():
@@ -16,6 +17,10 @@ def get_data():
 
 plot_interval = 1  # Plot every iteration
 max_iterations = 10000000
+tilt_angles = np.arange(-30, 31, 1)  # Tilt angles from -30 to 30 degrees
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
 
 for i in range(max_iterations):
     if (i % plot_interval == 0):
@@ -26,10 +31,11 @@ for i in range(max_iterations):
 
     print(i)
     current_data = get_data()
-    for point in current_data:
+    for idx, point in enumerate(current_data):
         x = point[2] * np.cos(np.radians(point[1]))  # Convert to Cartesian coordinates
         y = point[2] * np.sin(np.radians(point[1]))
-        z = 0  # Assuming lidar data is 2D (no height information)
+        tilt_angle = tilt_angles[idx]  # Get corresponding tilt angle
+        z = point[2] * np.tan(np.radians(tilt_angle))  # Compute z-coordinate based on tilt angle
 
         azimuth = np.arctan2(y, x)  # Azimuthal angle
         polar_angle = np.arccos(z / np.sqrt(x**2 + y**2 + z**2))  # Polar angle
@@ -41,12 +47,13 @@ for i in range(max_iterations):
         intensity.append(point[1])  # Use distance (or another appropriate value) as intensity
 
     if i % plot_interval == 0 and azimuths and polar_angles and distances:
-        plt.clf()
-        ax = plt.subplot(111, polar=True)
-        sc = ax.scatter(azimuths, distances, c=intensity, cmap='viridis', marker='o', alpha=0.5)
+        ax.cla()
+        sc = ax.scatter(azimuths, polar_angles, distances, c=intensity, cmap='viridis', marker='o', alpha=0.5)
+        ax.set_xlabel('Azimuthal Angle')
+        ax.set_ylabel('Polar Angle')
+        ax.set_zlabel('Radius')
         plt.title('RPLidar Data (Spherical Coordinates)')
         plt.colorbar(sc, label='Intensity')
-        plt.grid(True)
         plt.pause(0.1)
 
 plt.show(block=False)
