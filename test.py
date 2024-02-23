eb80cb7f93ab859271aa394f9e137cc6505829cf
Julia Piascik <98356859+liapia99@users.noreply.github.com>
2024-02-23 15:32:06 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from rplidar import RPLidar, RPLidarException
 
-max_iterations = 10  # or any desired value
-plot_interval = 5  # set to 30 for saving every 30 iterations
+def get_data():
+    try:
+        lidar = RPLidar('/dev/ttyUSB0')
+        for scan in lidar.iter_scans(max_buf_meas=500):
+            break
+        lidar.stop()
+        lidar.disconnect()
+        return scan
+    except RPLidarException as e:
+        print(f"Error: {e}")
+        return []
+
+max_iterations = 20  # or any desired value
+plot_interval = 1  # set to 30 for saving every 30 iterations
 
 # Lists to store data for each 30 iterations
 all_angles = []
@@ -29,14 +42,14 @@ fig = plt.figure()
 ax3d = fig.add_subplot(111, projection='3d')
 
 for i, (angles, distances) in enumerate(zip(all_angles, all_distances)):
-    x = np.cos(angles) * distances
-    y = np.sin(angles) * distances
+    x = distances * np.cos(np.radians(angles)) 
+    y = distances * np.sin(np.radians(angles))
     z = np.full_like(distances, i * plot_interval)
-    ax3d.scatter(x, y, z, cmap='green', marker='o', alpha=0.5)
+    ax3d.scatter(x, y, z, cmap='Pink', marker='o', alpha=0.5)
 
 ax3d.set_xlabel('X')
 ax3d.set_ylabel('Y')
-ax3d.set_zlabel('Z (Iteration)')
+ax3d.set_zlabel('Z ')
 plt.title('3D Plot')
 plt.grid(True)
 plt.show()
