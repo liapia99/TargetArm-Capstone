75e935be71b4acd1f96a89819a311f6ac6518ec2
Julia Piascik <98356859+liapia99@users.noreply.github.com>
2024-02-23 16:30:57 -0500

    Update test.py

--- test.py
+++ test.py
@@ -2,11 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from rplidar import RPLidar, RPLidarException
+from sensor_msgs.msg import LaserScan
 
+rospy.Subscriber("/asc"+"/scan", LaserScan, self.laserScanSubscriberCallback)
+
+def laserScanSubscriberCallback(self, msg):
+    rospy.loginfo("Message receieved with angle_min = " + str(msg.angle_min) + " [rad], angle_max = " + str(msg.angle_max) + " [rad], range_min = " + str(msg.range_min) + " [m], range_max = " + str(msg.range_max) + " [m]")
+print(laserScanSubscriberCallback(self,msg))
 def get_data():
     try:
         lidar = RPLidar('/dev/ttyUSB0')
         for scan in lidar.iter_scans(max_buf_meas=500):
+            for angle, distance in scan:
+                print(angle, distance)
             break
         lidar.stop()
         lidar.disconnect()
